6e007a1eff840090cacb0ffaab0208d58880b006
Simba Wangu <simbawangulu@gmail.com>
2023-01-23 13:45:30 +0300

    add feedback model, controller and route generated js files to eslintignore

--- src/models/Feedback.js
+++ src/models/Feedback.js
@@ -1,21 +1,21 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FeedbackModel = void 0;
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.FeedbackModel = void 0
 /* eslint-disable @typescript-eslint/no-var-requires */
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const FeedbackSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    emoji: {
-        type: String,
-        required: true
-    },
-    feedback: {
-        type: String,
-        required: true
-    }
-});
-const FeedbackModel = mongoose.model('Feedback', FeedbackSchema);
-exports.FeedbackModel = FeedbackModel;
+  username: {
+    type: String,
+    required: true
+  },
+  emoji: {
+    type: String,
+    required: true
+  },
+  feedback: {
+    type: String,
+    required: true
+  }
+})
+const FeedbackModel = mongoose.model('Feedback', FeedbackSchema)
+exports.FeedbackModel = FeedbackModel
